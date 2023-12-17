21aacddec12908779e3083715776451a2ad2d683
Deaths-Door <aaravaditya51@gmail.com>
2023-12-17 18:08:26 +0100

    Update

--- experiments/check_horizontal_vertical_lines_v2/main.py
+++ experiments/check_horizontal_vertical_lines_v2/main.py
@@ -41,6 +41,36 @@ def is_vertical(board : Board,player : Symbol) -> bool :
             return True    
     return False
 
+def is_horizontal(board : Board,player : Symbol) -> bool :
+    # So its avoided
+    if board.in_row_to_win == board.dimensions : 
+        return board.has_won(player) 
+    
+    COL_DEFAULT = 0
+    col = COL_DEFAULT
+    row = 0
+
+    IN_ROW_DEFAULT = 1
+    in_row_till_now = IN_ROW_DEFAULT
+
+    while row < board.dimensions :
+        #TODO : Maybe can optimize by checking if at point where a win is not possible eg dimensions/3 = 1.5 so 2nd square and then if its X or empty then O can't win , can technically drastically cut down on number of iters posssible  but not in cases like dimesinos = 5 and inrowtowin = 2 then no point
+        if board.board[row][col] == player :
+            in_row_till_now += 1
+
+        col += 1
+
+        # means we've reach end of the row so check next one
+        if not col < board.dimensions - 2 :
+            row += 1
+            col = COL_DEFAULT
+            in_row_till_now = IN_ROW_DEFAULT
+
+        if in_row_till_now == board.in_row_to_win:
+            return True  
+        
+    return False
+
 """
 def setup_board(grid : list[list[Symbol | None]],dimensions : int,in_row_to_win : int) -> Board :
     board = Board(dimensions,in_row_to_win)
---
+++ experiments/check_horizontal_vertical_lines_v2/tests_horizontal.py
@@ -0,0 +1,200 @@
+import unittest
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+from charlotte import Board,Symbol, X , O , E
+from experiments.check_horizontal_vertical_lines_v2.main import is_horizontal;
+
+def setup_board(grid : list[list[Symbol | None]],dimensions : int,in_row_to_win : int) -> Board :
+    board = Board(dimensions,in_row_to_win)
+    board.board = grid
+    return board
+
+class TestHorizontalWins(unittest.TestCase) :
+    def test_example(self) :
+        board = setup_board([
+            [X,O,O],
+            [X,E,E],
+            [O,O,O],
+        ],3,3)
+
+        self.assertTrue(is_horizontal(board,O))
+
+    def test_two_in_a_row_horizontal_1(self):
+        board = setup_board([
+            [E, X, X],
+            [E, E, E],
+            [X, X, X],
+        ],3,3)
+
+        self.assertTrue(is_horizontal(board,X))
+
+    def test_one_in_a_row_horizontal(self):
+        board = setup_board([
+            [E, E, E],
+            [X, E, E],
+            [E, E, E],
+        ],3,3)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_horizontal_win_with_mixed_symbols_1(self):
+        board = setup_board([
+            [X, O, X],
+            [O, X, E],
+            [E, X, E],
+        ],3,3)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_no_win_horizontal(self):
+        board = setup_board([
+            [E, E, E],
+            [O, E, E],
+            [E, E, X],
+        ],3,3)
+
+        self.assertFalse(is_horizontal(board,O))
+
+    def test_example_5x5(self):
+        board = setup_board([
+            [X, O, O, O, O],
+            [X, E, E, E, O],
+            [O, E, O, E, E],
+            [O, X, O, O, E],
+            [O, O, O, X, O],
+        ],5,3)
+
+        self.assertTrue(is_horizontal(board,O))
+
+    def test_three_in_a_row_horizontal_1_5x5(self):
+        board = setup_board([
+            [E, X, X, X, E],
+            [E, O, E, E, E],
+            [E, O, O, O, E],
+            [E, O, E, E, E],
+            [E, E, E, E, E],
+        ],5,3)
+
+        self.assertTrue(is_horizontal(board,X))
+
+    def test_two_in_a_row_horizontal_1_5x5(self):
+        board = setup_board([
+            [E, O, O, O, E],
+            [E, O, X, E, E],
+            [E, X, E, E, E],
+            [E, E, E, E, E],
+            [E, E, E, E, E],
+        ],5,3)
+
+        self.assertTrue(is_horizontal(board,O))
+
+    def test_five_in_a_row_horizontal_1_5x5(self):
+        board = setup_board([
+            [E, O, X, X, X, X],
+            [E, E, O, E, E, E],
+            [E, E, O, E, E, E],
+            [E, E, X, E, E, E],
+            [E, E, E, E, E, E],
+        ],5,3)
+
+        self.assertFalse(is_horizontal(board,O))
+
+    def test_no_win_horizontal_5x5(self):
+        board = setup_board([
+            [E, E, E, E, E],
+            [E, O, E, E, E],
+            [E, O, E, E, E],
+            [E, O, E, E, E],
+            [E, X, E, E, E],
+        ],5,3)
+
+        self.assertFalse(is_horizontal(board,O))
+
+    def test_example_3x3_in_row_to_win_2(self):
+        board = setup_board([
+            [X, X, X],
+            [O, E, O],
+            [O, E, O],
+        ],3,3)
+
+        self.assertTrue(is_horizontal(board,X))
+
+    def test_three_in_a_row_horizontal_1_3x3_in_row_to_win_2(self):
+        board = setup_board([
+            [E, X, X],
+            [E, O, E],
+            [E, E, O],
+        ],3,3)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_custom_inrowtowin(self) :
+        board = setup_board([
+            [E, E, E, E, E],
+            [E, O, O, O, O],
+            [E, E, E, E, E],
+            [E, E, E, E, E],
+            [E, E, E, E, E],
+        ],5,4)
+
+        self.assertTrue(is_horizontal(board,O))
+
+    def test_real_scenario_1(self):
+        board = setup_board([
+            [E, E, X, O, X, E],
+            [E, E, O, O, E, E],
+            [E, E, X, E, E, X],
+            [E, O, E, E, E, X],
+            [E, E, E, E, E, E],
+        ], 5, 3)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_real_scenario_2(self):
+        board = setup_board([
+            [E, E, E, E, E, E],
+            [E, E, O, E, E, E],
+            [E, O, X, E, E, E],
+            [E, O, O, X, E, E],
+            [E, O, O, O, X, E],
+        ], 5, 4)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_real_scenario_3(self):
+        board = setup_board([
+            [E, E, E, E, E, O],
+            [E, E, E, E, E, X],
+            [E, E, X, X, X, E],
+            [E, O, O, E, E, O],
+            [E, O, E, E, E, O],
+        ], 5, 5)
+
+        self.assertFalse(is_horizontal(board,X))
+
+    def test_real_scenario_4(self):
+        board = setup_board([
+            [E, E, E, E, E, O],
+            [E, E, E, E, E, X],
+            [E, E, O, O, O, E],
+            [E, O, O, X, O, O],
+            [E, O, E, O, O, O],
+        ], 5, 4)
+
+        self.assertFalse(is_horizontal(board,O))
+
+    def test_real_scenario_5(self):
+        board = setup_board([
+            [X, X, E, E, E, E],
+            [O, O, O, E, E, E],
+            [E, X, O, E, E, E],
+            [O, E, E, E, E, E],
+            [E, E, E, E, E, E],
+        ], 5, 3)
+
+        self.assertFalse(is_horizontal(board,X))
+
+if __name__ == '__main__':
+    unittest.main()
